d9c05b1654b294c26f83c4a8a829ed99a296bd63
JuanJo Lurbe <juanjose.lurbe@garajedeideas.com>
2024-07-08 10:58:33 +0200

    feat: shared library components

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/hashUtils';
+export * from './lib/error';
---
+++ libs/shared/src/lib/errors/index.ts
@@ -0,0 +1,4 @@
+export * from './error-utils';
+export * from './constants';
+export * from './models';
+export * from './types';
